531e16a5d8113bb2db49a819a8a975e9b09bf41e
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-01-05 18:17:54 +0900

    Add exception

--- fixed-github-header.js
+++ fixed-github-header.js
@@ -1,10 +1,15 @@
 window.onload = function () {
-  var header = document.getElementsByClassName('header')[0],
-    body     = document.getElementsByTagName('body')[0];
+  try {
+    var header = document.getElementsByClassName('header')[0],
+        body     = document.getElementsByTagName('body')[0];
+    if (!header || !body) return;
 
-  header.style.position = 'fixed';
-  header.style.top      = 0;
-  header.style.zIndex   = 1;
-  header.style.width    = '100%';
-  body.style.marginTop  = header.offsetHeight + 'px';
+    header.style.position = 'fixed';
+    header.style.top      = 0;
+    header.style.zIndex   = 1;
+    header.style.width    = '100%';
+    body.style.marginTop  = header.offsetHeight + 'px';
+  } catch (e) {
+    return;
+  }
 };
