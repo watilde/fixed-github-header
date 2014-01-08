0d3ca22c25c65f7ee6ce59bba2443e495f74e361
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-01-09 02:29:06 +0900

    Update z-index

--- fixed-github-header.js
+++ fixed-github-header.js
@@ -6,7 +6,7 @@ window.onload = function () {
 
     header.style.position = 'fixed';
     header.style.top      = 0;
-    header.style.zIndex   = 1;
+    header.style.zIndex   = 10;
     header.style.width    = '100%';
     body.style.marginTop  = header.offsetHeight + 'px';
   } catch (e) {
